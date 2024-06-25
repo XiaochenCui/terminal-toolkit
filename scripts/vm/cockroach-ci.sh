992aed3b1a124e7957032dc5806c6a650e159101
Xiaochen Cui <jcnlcxc.new@gmail.com>
2024-06-25 09:34:12 -0700

    update

--- scripts/vm/cockroach-ci.sh
+++ scripts/vm/cockroach-ci.sh
@@ -9,6 +9,15 @@ WORKSPACE=$HOME/code
 mkdir -p $WORKSPACE
 
 cd $WORKSPACE
+wget https://github.com/bazelbuild/bazelisk/releases/download/v1.20.0/bazelisk-linux-amd64
+chmod +x bazelisk-linux-amd64
+sudo ln -sf $WORKSPACE/bazelisk-linux-amd64 /usr/local/bin/bazel
+sudo apt-get install -y gcc
+
+# https://stackoverflow.com/questions/73529401/cannot-execute-cc1plus-execvp-no-such-file-or-directory
+sudo apt-get install -y --reinstall g++-13-x86-64-linux-gnu
+
+cd $WORKSPACE
 rm -rf cockroach
 git clone --recurse-submodules --depth 1 --branch debug https://github.com/XiaochenCui/cockroach.git
 
@@ -18,11 +27,6 @@ echo 'build --config=dev' >> .bazelrc.user
 echo 'build --config=lintonbuild' >> .bazelrc.user
 echo 'test --test_tmpdir=/tmp/cockroach' >> .bazelrc.user
 echo 'build --remote_cache=http://127.0.0.1:9867' >> .bazelrc.user
-
-cd $WORKSPACE
-wget https://github.com/bazelbuild/bazelisk/releases/download/v1.20.0/bazelisk-linux-amd64
-chmod +x bazelisk-linux-amd64
-sudo ln -sf $WORKSPACE/bazelisk-linux-amd64 /usr/local/bin/bazel
 
 cd $WORKSPACE/cockroach
 ./dev doctor
