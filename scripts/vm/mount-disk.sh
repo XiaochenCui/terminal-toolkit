b05ae557c470fb551522ea1f167edab191fc3142
Xiaochen Cui <jcnlcxc.new@gmail.com>
2024-07-07 13:04:33 -0700

    update

--- scripts/vm/mount-disk.sh
+++ scripts/vm/mount-disk.sh
@@ -10,6 +10,12 @@ set -o pipefail
 DISK_PATH=/dev/sdb
 MOUNT_PATH=/mnt/disks/medium
 
+# exit if the disk is already mounted
+if mount | grep $DISK_PATH; then
+  echo "The disk is already mounted."
+  exit 0
+fi
+
 # format the disk
 sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard $DISK_PATH
 
