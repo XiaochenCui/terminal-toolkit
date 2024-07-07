bf356caeb5d74a954d3027b9015b26198b61d04a
Xiaochen Cui <jcnlcxc.new@gmail.com>
2024-07-07 14:00:22 -0700

    update

--- scripts/install/ubuntu.sh
+++ scripts/install/ubuntu.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-# wget -O - https://raw.githubusercontent.com/XiaochenCui/terminal-toolkit/main/scripts/install/ubuntu.sh | bash
+# ========================================
+# Usage
+# ========================================
+
+# wget --no-cache -O - https://raw.githubusercontent.com/XiaochenCui/terminal-toolkit/main/scripts/install/ubuntu.sh | bash
+
+# ========================================
+# Bash Options
+# ========================================
 
 set -o xtrace
 set -o errexit
@@ -9,7 +17,15 @@ set -o pipefail
 # we allow unset variables for the sick of "z.sh"
 # set -o nounset
 
+# ========================================
+# Apt update
+# ========================================
+
 sudo apt-get update -y
+
+# ========================================
+# ZSH (install manually)
+# ========================================
 
 # install zsh
 # sudo apt-get install -y zsh
@@ -17,15 +33,30 @@ sudo apt-get update -y
 # install oh-my-zsh
 # sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install tmux
+# ========================================
+# tmux
+# ========================================
+
 sudo apt-get install -y tmux
+
+# ========================================
+# init workspace
+# ========================================
 
 WORKSPACE=$HOME/code
 mkdir -p $WORKSPACE
 
+# ========================================
+# terminal-toolkit
+# ========================================
+
 cd $WORKSPACE
 rm -rf terminal-toolkit
 git clone --depth=1 https://github.com/XiaochenCui/terminal-toolkit.git 
+
+# ========================================
+# activate xiaochen-rc
+# ========================================
 
 XIAOCHEN_RC="$HOME"/code/terminal-toolkit/scripts/entry/xiaochen-rc
 
